192512a14dffa3714e9a09cc62dab27c814dda65
Rubin Peci <rubinpeci@gmail.com>
2018-12-12 19:31:23 -0500

    Add stuff to control

--- control.c
+++ control.c
@@ -3,10 +3,14 @@
 #include <string.h>
 #include <sys/shm.h>
 #include <fcntl.h>
+#include <semaphore.h>
 
 int main(int argc, char **argv)
 {
 	key_t key = 1000;
+	int sem_key = 1000;
+
+	int sem_id;
 	int shm_id;
 	char contents[100];
 	int file_desc;
@@ -18,6 +22,8 @@ int main(int argc, char **argv)
 			shm_id = shmget(key, 100, 0666 | IPC_CREAT);
 			file_desc = open("file", O_CREAT | O_TRUNC);
 			printf("Able to make the shared memory and open the file\n");
+			sem_id = semget(sem_key, 1, 0);
+
 		}
 	}
 	else
