14f304fb2d85fa7772a788968906d34c88f27d09
ychan3 <ychan3@stuy.edu>
2018-12-13 11:44:33 -0500

    added comments to main.c

--- main.c
+++ main.c
@@ -7,31 +7,26 @@
 #include <sys/shm.h>
 #include <fcntl.h>
 
-
-// A main writing program
-// Will attempt to access the resources using the semaphore
-// Once in, it should display the last line added to the file (use shared memory to contain the size of the last line).
-// Prompt the use for the next line.
-// Once a new line is read, write that to the file, update the shared memory and then release the semaphore
+// NOT TESTED YET PLS TEST LMK IN THEORY IT WORKS
 
 int main(){
 	int semkey = 1000;
-	int sem_id = semget(semkey, 1, 0);
+	int sem_id = semget(semkey, 1, 0); //Get semaphore's id
 	if (sem_id < 0){
 		printf("ERROR\n");
 	}
 	else{
-		if(semctl(sem_id, 0, GETVAL, 0)){
+		if(semctl(sem_id, 0, GETVAL, 0)){ //If the value in the semaphore is not 0...
 			sembut operation;
 			operation.sem_num = 0;
 			operation.sem_op = -1;
 			operation.sem_flg = SEM_UNDO;
-			semop(sem_id, operation, 1);
+			semop(sem_id, operation, 1); //Decrease the semaphore by 1
 
 			int shmkey = 1000;
 			char * data;
 			shm_id = shmget(key, 100, 0666 | IPC_CREAT);
-			data = shmat(shmid, (void *)0, 0);
+			data = shmat(shmid, (void *)0, 0); //Put the shared data into char * data
 
 			if(!data[0]){
 		    printf("Segment was just created\n");
@@ -41,15 +36,15 @@ int main(){
 		  }
 
 			printf("Next line?\n");
-    	fgets(data,200,stdin);
+    	fgets(data,200,stdin); // Replace the shared data with the new data
     	data[strlen(data)-1] = 0;
 
 			file_desc = fopen("file", O_WRONLY | O_APPEND);
-			write(file_desc, data, strlen(data));
+			write(file_desc, data, strlen(data)); // Append the data into the file
 
-			if(!semctl(sem_id, 0, GETVAL, 0)){
+			if(!semctl(sem_id, 0, GETVAL, 0)){ //Check if the semaphore is at 0
 				operation.sem_op = 1;
-				semop(sem_id, operation, 1);
+				semop(sem_id, operation, 1); //Increase the semaphore back to 1
 			}
 		}
 	}
